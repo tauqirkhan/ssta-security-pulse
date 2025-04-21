565cf997bd55efe545303f8aba2d728d884a40fd
Tauqir <tauqirkhann.11@gmail.com>
2025-04-21 03:35:12 +0300

    fix: add new logo

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -37,7 +37,7 @@ const Navbar = () => {
       <div className="container mx-auto px-6 flex justify-between items-center">
         <Link to="/" className="flex items-center gap-2">
           <img
-            src="/lovable-uploads/a50ac7eb-2970-474e-8eb2-cf10408f00b9.png"
+            src="/logo.png"
             alt="SSTA Logo"
             className="h-12 w-auto max-w-[200px] object-contain transition-all duration-300 ease-in-out hover:opacity-80 sm:h-14 md:h-16"
             draggable="none"
